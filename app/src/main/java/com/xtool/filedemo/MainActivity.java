2c0db41b93400e18a94cecdcf7cd6a9ac193cf92
liu490099586 <ly490099@163.com>
2017-12-01 14:43:49 +0800

    update

--- app/src/main/java/com/xtool/filedemo/MainActivity.java
+++ app/src/main/java/com/xtool/filedemo/MainActivity.java
@@ -34,20 +34,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private List<Dir> roots;
     private int backPosition = 0;
 
-    private static String TAG = "MainActivity";
-    private WebView webView;
-    private String LOAD_URL = "http://192.168.1.58:36443/default.html?r=";
-    private String LOAD_PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
-    private String UPLOAD_URL = "http://192.168.1.58:36448/uploadfile/upload";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initData();
-        Random random = new Random(2);
-        Double d = random.nextDouble();
-        LOAD_URL = "http://192.168.1.58:36443/default.html?r=" + d;
         initView();
     }
 
@@ -73,39 +64,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         adapter = new DirListAdapter(this,dirMap.get(Dir.ROOTPATH));
         lv_dir.setAdapter(adapter);
 
-        webView = (WebView) findViewById(R.id.webview);
-
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);  //支持js
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setDefaultTextEncodingName("utf-8");
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);//设置允许js弹出alert对话框
-        webView.requestFocus();//触摸焦点起作用
-        webView.loadUrl(LOAD_URL);
-        webView.addJavascriptInterface(new GetFileImpl(), "getlocalFiles");
-
-        //设置了Alert才会弹出，重新onJsAlert（）方法return true可以自定义处理信息
-        webView.setWebChromeClient(new WebChromeClient() {
-            /**
-             * 处理alert弹出框
-             */
-            @Override
-            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
-                Log.e(TAG, "onJsAlert:" + message);
-                AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
-                b.setTitle("Alert");
-                b.setMessage(message);
-                b.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        result.confirm();
-                    }
-                });
-                b.setCancelable(false);
-                b.create().show();
-                return true;
-            }
-        });
     }
 
 
@@ -138,7 +96,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             //到达叶子节点，隐藏listview，显示webview
 
         }
-
 
     }
 
